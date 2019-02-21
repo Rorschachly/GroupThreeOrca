26c10c07282d2fcdf9c12f126f8b14be107ddb58
Sharanya Sudhakar <sharanu@uw.edu>
2019-02-21 06:50:46 -0800

    transitions work. keyboard triggered

--- state_unity/Assets/Scripts/Granny_Behavior.cs
+++ state_unity/Assets/Scripts/Granny_Behavior.cs
@@ -7,15 +7,44 @@ public class Granny_Behavior : MonoBehaviour {
     // Use this for initialization
 
     Animator aniamtor;
-    public static bool is_fish = false;
     int fishHash = Animator.StringToHash("give_fish");
+    int lookHash = Animator.StringToHash("lookat");
+    int endloonkHash = Animator.StringToHash("look_end");
+    int gofishHash = Animator.StringToHash("fish_trigger");
+    int nudge_trigger = Animator.StringToHash("nudge_trigger");
 	void Start () {
         aniamtor = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (is_fish)
-            aniamtor.SetTrigger(fishHash);
+            //aniamtor.SetTrigger(fishHash);
 	}
+
+    public void LookAt()
+    {
+        aniamtor.SetBool(lookHash, true);
+    }
+
+    public void LookAway()
+    {
+        aniamtor.SetTrigger(endloonkHash);
+        aniamtor.SetBool(lookHash, false);
+    }
+
+    public void GoFish()
+    {
+        aniamtor.SetTrigger(gofishHash);
+    }
+
+    public void GiveFish()
+    {
+        aniamtor.SetTrigger(fishHash);
+    }
+
+    public void NudgeFish()
+    {
+        aniamtor.SetTrigger(nudge_trigger);
+    }
+
 }
--- state_unity/Assets/Scripts/Player_Interaction.cs
+++ state_unity/Assets/Scripts/Player_Interaction.cs
@@ -6,15 +6,15 @@ public class Player_Interaction : MonoBehaviour {
 
 
     public GameObject XRRig;
-    public GameObject granny;
+    public GameObject granny, box;
     public float raycastlength;
     [SerializeField] LayerMask mylayermask;
-    bool isstop = false, isforward = true;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Vector3 granny_pos = new Vector3(granny.transform.position.x, XRRig.transform.position.y,
        granny.transform.position.z);
         XRRig.transform.LookAt(granny_pos);
+        
     }
 
     // Update is called once per frame
@@ -23,39 +23,57 @@ public class Player_Interaction : MonoBehaviour {
         Ray myray = new Ray(XRRig.transform.position, XRRig.transform.forward * raycastlength);
         RaycastHit hit;
 
-        if(Physics.Raycast(myray, out hit, raycastlength,mylayermask.value))
-        {
-            Vector3 granny_pos = granny.transform.position;
-            XRRig.transform.LookAt(granny_pos);
-        }
-
-        if(!isstop && isforward)
-            granny.transform.localPosition += new Vector3(0, 0, 0.08f);
-        if (!isstop&&!isforward)
-            granny.transform.Translate(-Vector3.forward*Time.deltaTime *5f, XRRig.transform);
-
-        if (granny.transform.position.z > 2 && isforward)
-        {
-            isforward = false;
-            granny.transform.LookAt(XRRig.transform.position);
-        }
-
-        //if (granny.transform.position.z < -15)
+        //if(Physics.Raycast(myray, out hit, raycastlength,mylayermask.value))
         //{
-        //    isstop = true;
-        //    granny_fish.transform.position = granny.transform.position;
-        //    granny_fish.gameObject.SetActive(true);
-        //    granny.gameObject.SetActive(false);
-        //    Granny_Behavior.is_fish = true;
+        //    Debug.Log("called from hit");
+        //   // hit.collider.GetComponent<Granny_Behavior>().LookAt();
         //}
-	}
+        //else
+        //{
+        //   // granny.GetComponent<Granny_Behavior>().LookAway();
+        //}
+
+        //for testing without oclus
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space");
+            granny.GetComponent<Granny_Behavior>().LookAt();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("box");
+            XRRig.transform.LookAt(box.transform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("F");
+            granny.GetComponent<Granny_Behavior>().LookAway();
+            granny.GetComponent<Granny_Behavior>().GoFish();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("G");
+            granny.GetComponent<Granny_Behavior>().LookAway();
+            granny.GetComponent<Granny_Behavior>().GiveFish();
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("N");
+            granny.GetComponent<Granny_Behavior>().LookAway();
+            granny.GetComponent<Granny_Behavior>().NudgeFish();
+        }
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "granny_swim")
         {
-            isstop = true;
-            Granny_Behavior.is_fish = true;
+            Debug.Log(other.name);
         }
 
     }
